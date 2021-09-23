ab05b90256cd6b281ff9313fa95b5a1990f3e1d9
lazyreader <kenvylf@gmail.com>
2021-09-23 10:42:34 +0800

    all functions complete, clean code and change iu left

--- Ledesma_Final/Ledesma_Final/Ledesma_Final.cpp
+++ Ledesma_Final/Ledesma_Final/Ledesma_Final.cpp
@@ -21,9 +21,13 @@ void addFriend(node*& head, node*& last, string name, int age, char gender, int 
 void viewFriend(node* cur);
 void viewAll(node* cur);
 void viewOne(node* cur);
-void deleteFriend(node*& head, string name);
-//void deleteAFriend(node*& head);//
+void deleteFriend(node*& head, node*& last, node* cur);
+void deleteAFriend(node*& head);
+void deleteMany(node*& head);
+void filterFriend(node* cur);
+void filterByGender(node* cur, char genderInput);
 
+int frCount = 0;
 
 int main()
 {
@@ -56,13 +60,13 @@ int main()
 			break;
 
 		case 'b':
+			filterFriend(head);
 			break;
 		case 'c':
 			viewFriend(head);
 			break;
 		case 'd':
-			cin >> name;
-			deleteFriend(head, name);
+			deleteFriend(head, last, head);
 			break;
 		}
 	} while (choice != 'e');
@@ -199,42 +203,152 @@ void viewAll(node* cur)
 }
 
 
-void deleteFriend(node*& head, string name)//wrong
+void deleteFriend(node*& head, node*& last, node* cur)
 {
-	node* tmp = head;
-	while (head->name == name)
+	if (emptyCheck(cur))
 	{
-		head = head->nextptr;
+		system("CLS");
+		cout << "FRIEND LIST EMPTY\n";
 	}
-	while (tmp->nextptr != NULL)
+	else
 	{
-		if (tmp->nextptr->name == name)
+		char delButton;
+		cout << "a. Delete via name search list\n";
+		cout << "b. Delete multiple friends via popularity input\n";
+		cin >> delButton;
+		switch (delButton)
 		{
-			tmp->nextptr = tmp->nextptr->nextptr;
-		}
-		else
-		{
-			tmp = tmp->nextptr;
+		case 'a':
+			deleteAFriend(head);
+			break;
+		case 'b':
+			deleteMany(head);
+			break;
 		}
 	}
 }
 
-/*void deleteAFriend(node*& head)
+void deleteAFriend(node*& head)
 {
 	string inputName;
+	bool searchList = false;
 	cout << "Input name of friend you want to delete :(\n";
+	cin.ignore();
 	getline(cin, inputName);
-	while (head != NULL)
+	node* current = new node;
+	node* prev = new node;
+	current = head;
+	cout << "--------verifying input------\n";
+	while (current != NULL)
 	{
-		if (inputName == head->name)
+		if (inputName == current->name)
 		{
-			node* current = new node;
-			node* previous = new node;
-			current = head;
-			previous = current;
-			current = current->nextptr;
-			previous->nextptr = current->nextptr;
+			searchList = true;
+			if (inputName == head->name)
+			{
+				head = current->nextptr;
+			}
+			else
+			{
+				prev->nextptr = current->nextptr;
+			}
+			cout << "name deleted, please check friends list again\n";
+		}
+		prev = current;
+		current = current->nextptr;
+		frCount--;
+	}
+	if (searchList == false)
+	{
+		cout << "Error; no friends with user-inputted name\n";
+	}
+}
+
+void deleteMany(node*& head)
+{
+	int rateInput;
+	bool ratingCheckList = false;
+	system("CLS");
+
+	cout << "Enter accurate rating number, program will unfriend people with the same rating\n";
+	cin >> rateInput;
+	node* current = new node;
+	node* prev = new node;
+	current = head;
+	cout << "Program searching friends with matching popularity rating...\n";
+	while (current != NULL)
+	{
+		if (rateInput == current->popularity)
+		{
+			if (rateInput == head->popularity)
+			{
+				head = current->nextptr;
+			}
+			else
+			{
+				prev->nextptr = current->nextptr;
+			}
+			frCount--;
+		}
+		prev = current;
+		current = current->nextptr;
+	}
+	if (ratingCheckList == false)
+	{
+		cout << "No friends with user-input popularity matches found\n";
+	}
+}
+
+void filterFriend(node* cur)
+{
+	char genderInput;
+	int rateInput;
+	if (emptyCheck(cur))
+	{
+		cout << "EMPTY FRIENDS\n";
+	}
+	else {
+		char selection;
+		cout << "g. Filter thru gender\n";
+		cout << "p. Filter thru popularity\n";
+		cin >> selection;
+		cin.ignore();
+		switch (selection)
+		{
+		case 'g':
+			cout << "input m for male or f for female\n";
+			cin >> genderInput;
+			cin.ignore();
+			filterByGender(cur, genderInput);
+			break;
+		case 'p':
+			cout << "input accurate popularity number\n";
+			cin >> rateInput;
+			cin.ignore();
+			//filterByPopularity(cur,rateInput);
+			break;
 		}
 	}
 }
-*/
+
+void filterByGender(node* cur, char genderInput)
+{
+	cout << "Filtering by Gender\n";
+	cout << "Loading...\n";
+	cout << "results\n";
+	while (cur != NULL)
+	{
+		if (genderInput == cur->gender)
+		{
+			cout << cur->name << endl;
+			cout << cur->age << endl;
+			cout << cur->gender << endl;
+			cout << cur->popularity << endl << endl;
+			cur = cur->nextptr;
+		}
+		else
+		{
+			cur = cur->nextptr;
+		}
+	}
+}
